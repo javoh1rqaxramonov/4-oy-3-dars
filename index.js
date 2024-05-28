b6f3e674db3e159e804cb41feb736a7590536ee9
javoh1rqaxramonov <ubotirov22@.com>
2024-05-28 20:02:34 +0500

    finished

---
+++ index.js
@@ -0,0 +1,67 @@
+// 1-masala
+// let a;
+// if (a>0) {
+//   console.log(true);
+// } else{
+//   console.log(false);
+// }
+// 2-masala
+// let a;
+// if (a%2==0) {
+//   console.log(true);
+  
+// } else {
+//   console.log(false);
+// }
+// 3-masala
+// let a;
+// if (a/2==0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// 4-masala
+// 
+// 5-masalalet a;
+// let s;
+// let a;
+// if (a>=0||b<-2) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// 7-masala
+// let q;
+// let a;
+// let x;
+// if ((b>a && b<c) || (b<a && b>c)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// 8-masala
+// let a;
+// let q;
+// if (a%2==0 && q%2==0) {
+//   console.log(true);
+// } else{
+//   console.log(false);
+// }
+// 9-masala
+// let a;
+// let s;
+// let counter=0;
+// if (a%2!==0 || s%2!==0) {
+//   console.log(true); 
+// }  else{
+//   console.log(false);
+// }
+// 10-masala
+// let a;
+// let s;
+// let counter=0;
+// if (a%2!==0 && s%2!==0) {
+//   console.log(true); 
+// }  else{
+//   console.log(false);
+// }
